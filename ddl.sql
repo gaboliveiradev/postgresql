403b866fe2a3f911a3f1f0e9e0122caf72b87cec
gaboliveiradev <g38789138@gmail.com>
2022-09-20 11:36:22 -0300

    first commit

---
+++ ddl.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE pgsql_teste;
+
+CREATE SEQUENCE id_email
+	INCREMENT 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1;
+	
+CREATE SEQUENCE id_funcionario
+	INCREMENT 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1;
+
+CREATE TABLE Email (
+	id int NOT NULL,
+	descricao varchar(100) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Funcionario (
+	id int NOT NULL,
+	nome varchar(100) NOT NULL,
+	id_email int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_email) REFERENCES Email (id)
+);
---
+++ inserts.sql
@@ -0,0 +1,9 @@
+-- Email
+INSERT INTO Email VALUES (nextval('id_email'), 'pgsql@teste.com');
+INSERT INTO Email VALUES (nextval('id_email'), 'gabriel@teste.com');
+INSERT INTO Email VALUES (nextval('id_email'), 'helisson@teste.com');
+
+-- Funcionário
+INSERT INTO Funcionario VALUES (nextval('id_funcionario'), 'Postgresql', 1);
+INSERT INTO Funcionario VALUES (nextval('id_funcionario'), 'Gabriel Oliveira', 2);
+INSERT INTO Funcionario VALUES (nextval('id_funcionario'), 'Helisson', 3);
